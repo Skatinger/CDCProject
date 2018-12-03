c202d2c50db35eb70c6bdea48351ce10c003ce09
alex <alex.nyffenegger@unifr.ch>
2018-12-03 19:51:32 +0100

    preparation to use cowboy

---
+++ src/cdcproject_app.erl
@@ -0,0 +1,17 @@
+-module(cdcproject_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+  io:format("IT WORKS~n"),
+  cdcproject_sup:start_link().
+
+stop(_State) ->
+  ok.
