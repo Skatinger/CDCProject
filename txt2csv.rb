7d6a734ef6356c535dcabf3e333ed4ef6c106954
alex <alex.nyffenegger@unifr.ch>
2018-12-17 00:43:42 +0100

    fixed unused code

--- txt2csv.rb
+++ txt2csv.rb
@@ -1,9 +1,7 @@
 csv = []
 File.open("simulation_results.txt", "r") do |f|
   f.each_line do |line|
-    # puts line
     csv << line.gsub(/(:|{|\[|,|\]|[a-zA-Z])/, "").gsub(/}/, ",")
-    puts csv
     end
 end
 
